40b4b951d451f538dbd337e9aca64d959911f98f
Gabriel Duarte <gabrielqduarte@gmail.com>
2025-05-20 00:03:38 +0200

    improve header spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ import { Operations } from "~/components/dashboard/operations";
 export default function HomePage() {
   return (
     <main className="container mx-auto max-w-5xl p-4">
-      <h1 className="font-title my-8 text-6xl font-bold">{"> "}BlockyUI</h1>
+      <h1 className="font-title mt-8 mb-16 text-6xl font-bold">
+        {"> "}BlockyUI
+      </h1>
 
       <div className="space-y-6">
         <div className="grid gap-6 md:grid-cols-2">
