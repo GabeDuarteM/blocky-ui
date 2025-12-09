da4828026099f60967d1573b17a2aaf1a31bc586
Gabriel Duarte <gabrielqduarte@gmail.com>
2025-12-09 22:15:16 +0100

    migrate other packages

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -15,23 +15,21 @@ export const contentType = "image/png";
 
 export default function Icon() {
   return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 28,
-          background: "#000000",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "#ffffff",
-          fontFamily: jersey15.style.fontFamily,
-        }}
-      >
-        {">"}
-      </div>
-    ),
+    <div
+      style={{
+        fontSize: 28,
+        background: "#000000",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#ffffff",
+        fontFamily: jersey15.style.fontFamily,
+      }}
+    >
+      {">"}
+    </div>,
     {
       ...size,
     },
