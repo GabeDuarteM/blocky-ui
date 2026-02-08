fbb35558ddd2090ac8dfdc5f614a045ad8efd0ac
Gabriel Duarte <gabrielqduarte@gmail.com>
2026-02-08 23:30:24 +0100

    fix drizzle studio schema location

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { type Config } from "drizzle-kit";
 import { env } from "./src/env.js";
 
 export default {
-  schema: "./src/server/logs/mysql-schema.ts",
+  schema: "./src/server/logs/mysql/schema.ts",
   dialect: "mysql",
   dbCredentials: {
     url: env.QUERY_LOG_TARGET ?? "",
